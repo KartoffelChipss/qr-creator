263f94be83cd3d14ea4ff0486db9e025bab18443
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2025-06-24 18:46:36 +0200

    Added Calendar Event QR Codes

--- src/components/features/input/ContentSection.tsx
+++ src/components/features/input/ContentSection.tsx
@@ -1,111 +1,107 @@
-import {FunctionalComponent} from "preact";
-import UrlInput from "./inputs/UrlInput.tsx";
-import TextInput from "./inputs/TextInput.tsx";
-import EMailInput from "./inputs/EMailInput.tsx";
-import SmsInput from "./inputs/SmsInput.tsx";
-import WiFiInput from "./inputs/WiFiInput.tsx";
-import {useState} from "preact/hooks";
-import VCardInput from "./inputs/VCardInput.tsx";
+import { FunctionalComponent } from 'preact';
+import UrlInput from './inputs/UrlInput.tsx';
+import TextInput from './inputs/TextInput.tsx';
+import EMailInput from './inputs/EMailInput.tsx';
+import SmsInput from './inputs/SmsInput.tsx';
+import WiFiInput from './inputs/WiFiInput.tsx';
+import { useState } from 'preact/hooks';
+import VCardInput from './inputs/VCardInput.tsx';
+import CalendarInput from './inputs/CalendarInput.tsx';
 
-type ContentSection = "url" | "text" | "vcard" | "email" | "sms" | "wifi";
+type ContentSection =
+    | 'url'
+    | 'text'
+    | 'vcard'
+    | 'email'
+    | 'sms'
+    | 'wifi'
+    | 'calendar';
 
-const ContentSection: FunctionalComponent<{ onSubmit?: (input: string) => void }> = ({ onSubmit }) => {
-    const [section, setSection] = useState<ContentSection>("url");
+const ContentSection: FunctionalComponent<{
+    onSubmit?: (input: string) => void;
+}> = ({ onSubmit }) => {
+    const [section, setSection] = useState<ContentSection>('url');
 
     const submitData = (input: string) => {
         if (onSubmit) onSubmit(input);
-    }
+    };
 
     return (
         <>
             <nav>
                 <button
-                    onClick={() => setSection("url")}
-                    className={section === "url" ? "active" : ""}
+                    onClick={() => setSection('url')}
+                    className={section === 'url' ? 'active' : ''}
                 >
                     URL
                 </button>
                 <button
-                    onClick={() => setSection("text")}
-                    className={section === "text" ? "active" : ""}
+                    onClick={() => setSection('text')}
+                    className={section === 'text' ? 'active' : ''}
                 >
                     Text
                 </button>
                 <button
-                    onClick={() => setSection("vcard")}
-                    className={section === "vcard" ? "active" : ""}
+                    onClick={() => setSection('wifi')}
+                    className={section === 'wifi' ? 'active' : ''}
+                >
+                    WiFi
+                </button>
+                <button
+                    onClick={() => setSection('vcard')}
+                    className={section === 'vcard' ? 'active' : ''}
                 >
                     vCard
                 </button>
                 <button
-                    onClick={() => setSection("email")}
-                    className={section === "email" ? "active" : ""}
+                    onClick={() => setSection('calendar')}
+                    className={section === 'calendar' ? 'active' : ''}
+                >
+                    Calendar Event
+                </button>
+                <button
+                    onClick={() => setSection('email')}
+                    className={section === 'email' ? 'active' : ''}
                 >
                     Email
                 </button>
                 <button
-                    onClick={() => setSection("sms")}
-                    className={section === "sms" ? "active" : ""}
+                    onClick={() => setSection('sms')}
+                    className={section === 'sms' ? 'active' : ''}
                 >
                     SMS
                 </button>
-                <button
-                    onClick={() => setSection("wifi")}
-                    className={section === "wifi" ? "active" : ""}
-                >
-                    WiFi
-                </button>
             </nav>
 
-            {
-                section === "url" && (
-                    <UrlInput
-                        onInput={(input) => submitData(input)}
-                    />
-                )
-            }
+            {section === 'url' && (
+                <UrlInput onInput={(input) => submitData(input)} />
+            )}
 
-            {
-                section === "text" && (
-                    <TextInput
-                        onInput={(input) => submitData(input)}
-                    />
-                )
-            }
+            {section === 'text' && (
+                <TextInput onInput={(input) => submitData(input)} />
+            )}
 
-            {
-                section === "vcard" && (
-                    <VCardInput
-                        onInput={(input) => submitData(input)}
-                    />
-                )
-            }
+            {section === 'wifi' && (
+                <WiFiInput onInput={(input) => submitData(input)} />
+            )}
 
-            {
-                section === "email" && (
-                    <EMailInput
-                        onInput={(input) => submitData(input)}
-                    />
-                )
-            }
+            {section === 'vcard' && (
+                <VCardInput onInput={(input) => submitData(input)} />
+            )}
 
-            {
-                section === "sms" && (
-                    <SmsInput
-                        onInput={(input) => submitData(input)}
-                    />
-                )
-            }
+            {section === 'calendar' && (
+                <CalendarInput onInput={(input) => submitData(input)} />
+            )}
 
-            {
-                section === "wifi" && (
-                    <WiFiInput
-                        onInput={(input) => submitData(input)}
-                    />
-                )
-            }
+            {section === 'email' && (
+                <EMailInput onInput={(input) => submitData(input)} />
+            )}
+
+            {section === 'sms' && (
+                <SmsInput onInput={(input) => submitData(input)} />
+            )}
         </>
-    )
-}
+    );
+};
 
 export default ContentSection;
---
+++ src/components/features/input/inputs/CalendarInput.tsx
@@ -0,0 +1,72 @@
+import { FunctionalComponent } from 'preact';
+import { CustomInputProps } from './interfaces.ts';
+import { useState } from 'preact/hooks';
+import { RefreshCcwAlt1 } from 'dazzle-icons/src';
+
+const formatToICalDate = (localDate: string): string => {
+    const date = new Date(localDate);
+    return date
+        .toISOString()
+        .replace(/[-:]/g, '')
+        .replace(/\.\d{3}Z$/, 'Z');
+};
+
+const CalendarInput: FunctionalComponent<CustomInputProps> = ({ onInput }) => {
+    const [summary, setSummary] = useState<string>('');
+    const [dtStart, setDTStart] = useState<string>('');
+    const [dtEnd, setDTEnd] = useState<string>('');
+    const [location, setLocation] = useState<string>('');
+
+    const handleSubmit = () => {
+        if (onInput)
+            onInput(
+                `BEGIN:VEVENT\nSUMMARY:${summary}\nDTSTART:${dtStart}\nDTEND:${dtEnd}\nLOCATION:${location}\nEND:VEVENT`
+            );
+    };
+
+    return (
+        <>
+            <h3>Calendar Event</h3>
+            <p>
+                Generate a QR-Code that will add a calendar event with the given
+                details to the user's calendar.
+            </p>
+            <label>Summary</label>
+            <input
+                type={'text'}
+                onInput={(e) => setSummary(e.currentTarget.value)}
+                placeholder={'This is a calendar event'}
+            />
+
+            <label>Start Date & Time</label>
+            <input
+                type="datetime-local"
+                onInput={(e) =>
+                    setDTStart(formatToICalDate(e.currentTarget.value))
+                }
+            />
+
+            <label>End Date & Time</label>
+            <input
+                type="datetime-local"
+                onInput={(e) =>
+                    setDTEnd(formatToICalDate(e.currentTarget.value))
+                }
+            />
+
+            <label>Location</label>
+            <input
+                type={'text'}
+                onInput={(e) => setLocation(e.currentTarget.value)}
+                placeholder={'Office, Conference Room 1'}
+            />
+
+            <button onClick={() => handleSubmit()} className={'blue'}>
+                <RefreshCcwAlt1 />
+                Generate
+            </button>
+        </>
+    );
+};
+
+export default CalendarInput;
